00ae18be314293558a97ec7b5831840503309290
Uģis Ozols <ugis.ozolss@gmail.com>
2010-07-09 21:37:24 +0300

    first init.

---
+++ lib/kata.rb
@@ -0,0 +1,3 @@
+class Kata
+  
+end
---
+++ test/kata_test.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH.unshift(File.join(File.dirname('__FILE__'), '·.', 'lib'))
+
+require 'test/unit'
+require 'kata'
+
+class KataTests < Test::Unit::TestCase
+  
+end
